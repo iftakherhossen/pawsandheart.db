2ab12e8cf54a9ed872041c75ad1f8e9c7e1047f6
Iftakher Hossen <you@example.comiftakher.one@gmail.com>
2024-07-10 22:28:30 +0600

    cors added

--- pawsandheart/settings.py
+++ pawsandheart/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://pawsandheart-db.onrender.com', 'https://*.127.0.0.1']
+CORS_ALLOWED_ORIGINS = ['https://pawsandheart-db.onrender.com', 'https://*.127.0.0.1', 'http://127.0.0.1:5500']
 
 # Application definition
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
