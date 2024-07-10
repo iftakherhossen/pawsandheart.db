0f317cf7796d7f1ec927810f86a4ca675cfba55f
Iftakher Hossen <you@example.comiftakher.one@gmail.com>
2024-07-10 20:25:17 +0600

    second commit

--- pawsandheart/urls.py
+++ pawsandheart/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pawsandheart project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('contact.urls')),
+    path('', include('transaction.urls')),
+    path('pets/', include('pet.urls')),
+    path('', include('user.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
